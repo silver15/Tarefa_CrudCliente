4028471761edaa97e92c157b49343681e229ef27
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-11-29 18:55:47 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/CrudClientes/services/ClienteService.java
+++ backend/src/main/java/com/devsuperior/CrudClientes/services/ClienteService.java
@@ -1,16 +1,15 @@
 package com.devsuperior.CrudClientes.services;
 
-import java.nio.file.DirectoryNotEmptyException;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
-import org.hibernate.ResourceClosedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,9 +26,9 @@ public class ClienteService {
 	private ClienteRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClienteDTO> findAll() {
-		List<Cliente> list = repository.findAll();
-		return list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
+	public Page<ClienteDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Cliente> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClienteDTO(x));
 
 	}
 	
